a7df576f4a00b6c16bade77d8ecfd1e52e758d0b
lSATAl <70383465+lSATAl@users.noreply.github.com>
2020-10-25 12:33:41 +0300

    init: add soluttion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for(let i = 0; i < 50; i++){
+        str = str.replace(/\(\)/g, '');
+        str = str.replace(/\{\}/g, '');
+        str = str.replace(/\[\]/g, '');
+        str = str.replace(/\|\|/g, '');
+        str = str.replace(/12/, '');
+        str = str.replace(/34/, '');
+        str = str.replace(/56/, '');
+        str = str.replace(/77/, '');
+        str = str.replace(/88/, '');
+    }
+    if (str !== '') {
+        return false
+    } else {
+        return true
+    }
 }
